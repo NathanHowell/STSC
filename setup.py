a00baf003f9f454d1e7b114950c55986ab4582de
Nathan Howell <nathan@sonai.ai>
2019-07-16 15:59:44 -0700

    Add setup.py for pip

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='stsc',
+    version='0.1dev',
+    packages=['stsc',],
+    license='Public Domain',
+    long_description=open('README.md').read(),
+)
